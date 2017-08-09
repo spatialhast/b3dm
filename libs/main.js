6da4402934948a07d209f7bac947e99f496e8757
Alexander <spatial.hast@gmail.com>
2017-08-09 20:05:06 +0300

    init

---
+++ libs/main.js
@@ -0,0 +1,12 @@
+Cesium.BingMapsApi.defaultKey = "AmpMgzJo6R6yUmEllLhbqHLYM5Jrza_bJkzpR0rXdZFYywlCPcbMI07S1eN5Qnvd";
+
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    animation: false,
+    baseLayerPicker: true,
+    geocoder: false,
+    imageryProvider: false,
+    homeButton: false,
+    navigationHelpButton: false,
+    timeline: false,
+    infoBox: false
+});
